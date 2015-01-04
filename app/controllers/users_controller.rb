a9326f42ddf7f0eb089dbb05113f23f432f92724
Harish Ramachandran <harishsr.coder@gmail.com>
2015-01-04 09:33:25 -0500

    7.2: Showing Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    # debugger # --> use this single line of code wherever and whenever debugging is necessary.  Check the rails server to interact and find the issue.
+  end
+
   def new
   end
+
 end
