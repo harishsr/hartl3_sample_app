9650b480351b70a453770bf9dc69b04508a31a2d
Harish Ramachandran <harishsr.coder@gmail.com>
2015-01-05 20:47:08 -0500

    7.2.1: Using form_for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    # debugger # --> use this single line of code wherever and whenever debugging is necessary.  Check the rails server to interact and find the issue.
   end
 
   def new
+    @user = User.new
   end
 
 end
