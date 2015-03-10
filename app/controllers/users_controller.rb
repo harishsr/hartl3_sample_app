b44c8eb811f43d1c783d05830bb79a20540ab7d9
Harish Ramachandran <harishsr.coder@gmail.com>
2015-03-09 20:27:49 -0400

    9.1.4: Successful edits (with TDD)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,9 +24,10 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.new(user_params)
+    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # Handle a successful update.
+      flash[:success] = "Profile updated."
+      redirect_to @user
     else
       render 'edit'
     end
