28daa3c27a78957d0b49a6e262770638ab5c64bd
Harish Ramachandran <harishsr.coder@gmail.com>
2015-04-16 17:49:02 -0400

    12.2.2: Stats and a follow form

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 # Users
-User.create!(name: "Example User",
-             email: "email@railstutorial.org",
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
              password:              "foobar",
-             password_confirmation: "foobar", 
-             admin: true,
+             password_confirmation: "foobar",
+             admin:     true,
              activated: true,
              activated_at: Time.zone.now)
 
 99.times do |n|
-  name = Faker::Name.name
+  name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
-  User.create(name: name,
+  User.create!(name: name,
               email: email,
               password:              password,
               password_confirmation: password,
@@ -28,8 +28,8 @@ end
 
 # Following relationships
 users = User.all
-user = User.first
+user  = users.first
 following = users[2..50]
-followers = useres[3..40]
+followers = users[3..40]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
