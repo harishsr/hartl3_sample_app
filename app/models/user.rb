e1ff0c519e54034e2f80e376b0d2c8a689138b10
Harish Ramachandran <harishsr.coder@gmail.com>
2015-04-20 14:22:17 -0400

    12.3.2: A first feed implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -78,10 +78,9 @@ class User < ActiveRecord::Base
     reset_sent_at < 2.hours.ago
   end
 
-  # Defines a proto-feed
-  # See 'Following Users' (chapter 12) for the full implementation
+  # Defines a user's status feed using SQL
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
   end
 
   # Follows a user
